e702e8c618a0293405199f7f43f4c247088e28e6
md-taha-ahmed <md.taha.ahmed.projects@gmail.com>
2022-05-13 11:59:34 +0530

    postman file

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// protected route
+Route::middleware('auth:sanctum')->delete('/logout', [UserController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// public route
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
